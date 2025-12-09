f6eefc1d08840601e0789f7fb6866041a44c67db
AiWithRajesh <choudharyrajesh729@gmail.com>
2025-12-10 01:31:29 +0530

    update: spiral matrix completed

--- array/spiralMatrix.js
+++ array/spiralMatrix.js
@@ -1,13 +1,48 @@
 const spiralSeries = (matrix) => {
-  spiralNumbers = [];
-  const set = [];
-  for (let i = 0; i < matrix.length; i++) {
-    currentArr = matrix[i];
-    const lastIndex = currentArr.length - 1;
-    set.push(lastIndex);
+  const m = matrix.length;
+  const n = matrix[0].length;
+
+  let visited = Array.from({ length: m }, () => Array(n).fill(false));
+
+  // Arrays to represent the changes in row and column
+  // indices: turn right(0, +1), turn down(+1, 0), turn
+  // left(0, -1), turn up(-1, 0)
+  const dr = [0, 1, 0, -1];
+  const dc = [1, 0, -1, 0];
+
+  // Initial position in the matrix
+  let r = 0,
+    c = 0,
+    idx = 0;
+
+  // Initial direction index
+  // let idx = 0;
+  const res = [];
+
+  for (let i = 0; i < m * n; ++i) {
+    res.push(matrix[r][c]);
+    visited[r][c] = true;
+
+    let newR = r + dr[idx];
+    let newC = c + dc[idx];
+
+    if (
+      newR >= 0 &&
+      newR < m &&
+      newC >= 0 &&
+      newC < n &&
+      !visited[newR][newC]
+    ) {
+      r = newR;
+      c = newC;
+    } else {
+      idx = (idx + 1) % 4;
+      r += dr[idx];
+      c += dc[idx];
+    }
   }
 
-  console.log(set);
+  return res;
 };
 
 const matrix = [
@@ -16,3 +51,4 @@ const matrix = [
   [7, 8, 9],
 ];
 const res = spiralSeries(matrix);
+console.log("res", res);
